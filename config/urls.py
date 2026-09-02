4150d847f06bf9e92129150eb9231b82445df05c
Behnam Dadashi <dadashi.contact@gmail.com>
2026-09-02 14:11:29 +0330

    Remove redoc endpoint

--- config/urls.py
+++ config/urls.py
@@ -19,27 +19,11 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
-
-    # raw openapi schema (yaml)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-
-    # swagger
-    path(
-        'api/schema/swagger',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger'
-    ),
-
-    # redoc
-    path(
-        'api/schema/redoc', 
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc'
-    ),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
