bce779915c101c67f4fda047df0de38955595065
Behnam Dadashi <dadashi.contact@gmail.com>
2026-09-02 14:06:40 +0330

    Set up swagger & redoc

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
+
+    # raw openapi schema (yaml)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # swagger
+    path(
+        'api/schema/swagger',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger'
+    ),
+
+    # redoc
+    path(
+        'api/schema/redoc', 
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'
+    ),
 ]
