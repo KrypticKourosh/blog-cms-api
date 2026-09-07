7bb6184410ce234487e2c60e471219056b8d30f9
Behnam Dadashi <kryptickourosh@gmail.com>
2026-09-07 15:57:25 +0330

    Add Comment model

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+# admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'post',
+        'author',
+        'created_at',
+        'parent',
+    ]
+    list_filter = [
+        'post',
+        'author',
+        'is_active',
+        'created_at',
+    ]
+    search_fields = [
+        'post__title',
+        'author__username',
+        'content',
+    ]
+    # uncomment on deploy
+    # raw_id_fields = (
+    #     'post',
+    #     'author',
+    #     'parent'
+    # )
--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
+from blog.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey( # nested replies
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='replies',
+    )
+    content = models.TextField()
+    # likes_count = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+        indexes = [
+            models.Index(fields=['post', 'author', 'created_at'])
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
