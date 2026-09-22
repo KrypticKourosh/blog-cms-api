4f8c38a64e7cbc80f24bd3da49d5c361a3313cff
Behnam Dadashi <kryptickourosh@gmail.com>
2026-09-22 17:14:06 +0330

    Add rate limiting

--- config/settings.py
+++ config/settings.py
@@ -157,6 +157,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "30/minute",      # Anonymous users
+        "user": "100/minute",     # Authenticated users
+    },
 }
 
 SPECTACULAR_SETTINGS = {
